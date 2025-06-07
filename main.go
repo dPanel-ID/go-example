cd3e977c9da4138a4abcb4b825a5bb50bcf2ac92
Prakasa <prakasa@devetek.com>
2025-06-07 23:30:35 +0700

    fix: example repo running multiple app in different port

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func httpHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,11 +13,12 @@ func httpHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var port = os.Getenv("PORT")
 	http.HandleFunc("/", httpHandler)
 
-	log.Println("Server started on :9000")
-	log.Println("Visit http://localhost:9000")
+	log.Println("Server started on :" + port)
+	log.Println("Visit http://localhost:" + port)
 	log.Println("Press Ctrl+C to stop the server")
 
-	http.ListenAndServe(":9000", nil)
+	http.ListenAndServe(":"+port, nil)
 }
