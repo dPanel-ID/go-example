4f36cb67620c67920d2cb409f45604eb1e0ef319
Prakasa <prakasa@devetek.com>
2026-02-11 09:40:49 +0700

    fix: use html template

--- main.go
+++ main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"runtime"
 )
 
+type osData struct {
+	Hostname     string
+	OS           string
+	Architecture string
+	CPUs         int
+	GoVersion    string
+}
+
 func httpHandler(w http.ResponseWriter, r *http.Request) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "unknown"
+	}
+
+	var data = osData{
+		Hostname:     hostname,
+		OS:           runtime.GOOS,
+		Architecture: runtime.GOARCH,
+		CPUs:         runtime.NumCPU(),
+		GoVersion:    runtime.Version(),
+	}
+	// Parse the template file
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+
+	// Execute the template, writing the output to the http.ResponseWriter
+	tmpl.Execute(w, data)
 
 	w.Header().Set("content-type", "text/html")
-	w.Write([]byte("Halo dunia, aplikasi ini ada di Raspberry Pi HomeLab tanpa dedicated IP public!"))
 }
 
 func main() {
